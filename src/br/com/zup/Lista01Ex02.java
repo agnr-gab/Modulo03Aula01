9ad433f5c48992c6b8ce19356d60343c16f826af
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-13 18:57:05 -0300

    iniciando exercicio 02

---
+++ src/br/com/zup/Lista01Ex02.java
@@ -0,0 +1,17 @@
+package br.com.zup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Lista01Ex02 {
+    public static void main(String[] args) {
+        /* 2. Crie um programa para gerenciar uma lista de candidatos de um vestibular, cada candidato tem número de matrícula e uma nota. No final do programa, o usuário deve decidir se irá adicionar mais de um candidato, exibir todos os candidatos cadastrados ou excluir um candidato pelo número de matrícula. */
+        Scanner leitorMatricula = new Scanner(System.in);
+        Map <Double, Double> = new HashMap<Double, Double>();
+
+
+
+
+    }
+}
