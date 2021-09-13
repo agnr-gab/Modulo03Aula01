be7b9e49940364ea14bd40922f9cec3731c173a4
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-13 14:06:16 -0300

    desenvolvimento do exercicio

--- src/br/com/zup/Lista01Ex01.java
+++ src/br/com/zup/Lista01Ex01.java
@@ -1,8 +1,30 @@
 package br.com.zup;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Lista01Ex01 {
 
     public static void main(String[] args) {
-	// write your code here
+        /*	1. Crie um programa para gerenciar uma lista de produtos de um estabelecimento, cada produto tem um nome e um preço. No final do programa, o usuário deve decidir se irá adicionar mais de um produto, exibir todos os produtos cadastrados ou excluir um produto pelo nome. */
+        Scanner leitorProdutos = new Scanner(System.in);
+        Map<String, Double> listaProdutos = new HashMap<String, Double>();
+
+        System.out.println("___Cadastro de Produtos___");
+        //Variáveis
+
+        while (true) {
+            System.out.println("Digite 1 para cadastrar produto.");
+            System.out.println("Digite 2 exibir lista de produtos cadastrados.");
+            System.out.println("Digite 3 excluir um produto.");
+            int opcaoMenu = leitorProdutos.nextInt();
+            leitorProdutos.nextInt();
+            switch (opcaoMenu) {
+                case 1:
+                    System.out.println("Digite o nome do produto");
+                    String nomeProduto = leitorProdutos.nextLine();
+            }
+        }
     }
 }
