475553b5eb84c3f07ea1ea69b7cc9725f80c18c0
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-13 21:43:30 -0300

    exercicio 03

---
+++ src/br/com/zup/Lista01Ex03.java
@@ -0,0 +1,16 @@
+package br.com.zup;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Lista01Ex03 {
+    public static void main(String[] args) {
+        /*   3. Crie um programa para gerenciar o cardápio de um restaurante. Cada prato tem um nome e pode conter diversos ingredientes. No final do programa, o usuário deve decidir se irá adicionar mais de um prato, exibir todos os pratos cadastrados ou excluir um prato pelo nome.   */
+        Scanner leitorCardapio = new Scanner(System.in);
+
+        HashMap<String, String> listaCardapio = new HashMap<String, String>();
+
+        System.out.println("Bem-vinde ao nosso restaurante! \nMonte seu prato!");
+
+    }
+}
