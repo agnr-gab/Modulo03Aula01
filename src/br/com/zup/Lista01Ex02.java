ea2e31e82037fc27947be96bb058ce56620bbbfd
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-13 21:07:20 -0300

    finalização do exercicio 02

--- src/br/com/zup/Lista01Ex02.java
+++ src/br/com/zup/Lista01Ex02.java
@@ -11,11 +11,11 @@ public class Lista01Ex02 {
 
         Map<Double, Double> listaMatricula = new HashMap<Double, Double>();
 
-        System.out.println("___Cadastro de Vestibular___");
+        System.out.println("___Cadastro do Vestibular___");
         int opcaoMenu = 0;
 
         do {
-            System.out.println("Escolha uma opção e digite o número correspondente:");
+            System.out.println("_Escolha uma opção e digite o número correspondente:_");
             System.out.println("Digite 1 para matricular candidate.");
             System.out.println("Digite 2 exibir lista de candidates cadastrades.");
             System.out.println("Digite 3 excluir canditade cadastrade.");
@@ -24,7 +24,7 @@ public class Lista01Ex02 {
             opcaoMenu = leitorMatricula.nextInt(); //opção que o usuário inseriu
             leitorMatricula.nextLine(); //limpar o buffer evita erro de entrada de dados
 
-            if (opcaoMenu == 1) {
+            if (opcaoMenu == 1) { //cadastro
                 System.out.println("Digite o número da matrícula do canditade:");
                 Double matriculaAlune = leitorMatricula.nextDouble();
                 System.out.println("Digite a nota do canditade:");
@@ -32,11 +32,11 @@ public class Lista01Ex02 {
                 listaMatricula.put(matriculaAlune, notaAlune);
                 System.out.println("___Canditade cadastrade com sucesso___");
             }
-            if (opcaoMenu == 2)//mostra a lista de dos produtos que o usuario já cadastrou
+            if (opcaoMenu == 2)//mostra a lista de canditades cadastrades
                 for (Double verificarLista : listaMatricula.keySet()) {
                     System.out.println("Produto: " + verificarLista + "valor R$ " + listaMatricula.get(verificarLista));
                 }
-            if (opcaoMenu == 3) {
+            if (opcaoMenu == 3) { //realiza a exclusão via número de matricula
                 System.out.println("Digite o numero da matrícula de canditade que deseja excluir da lista:"); //exclusão de um produto do cadastro
                 double matriculaDeletada = 0; //entrada do produto a ser deletado
                 double deletarMatricula = leitorMatricula.nextDouble();
@@ -46,7 +46,7 @@ public class Lista01Ex02 {
                 System.out.println("A exclusão da matrícula nº " +deletarMatricula+" foi realizada com sucesso !"); //mensagem de confirmção do produto que foi excluido
                 listaMatricula.remove(matriculaDeletada);
             }
-            if (opcaoMenu == 4) {
+            if (opcaoMenu == 4) { //encerra o programa
                 break;
             }
         } while (opcaoMenu >= 3);
